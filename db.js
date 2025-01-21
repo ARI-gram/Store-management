f65cea5fde7fe266789b3545e190552e379b6c16
samson kipruto kipkoech <samsonkips200@gmail.com>
2025-01-21 10:25:11 +0300

    new database

--- db.js
+++ db.js
@@ -1,24 +1,34 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-// Initialize PostgreSQL connection pool without SSL for local database
+// Determine if SSL is required based on the environment or database settings
+const isSSLRequired = process.env.DB_SSL === 'true';
+
+// Initialize PostgreSQL connection pool
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
-  ssl: false, // SSL not needed for local connections
+  ssl: isSSLRequired
+    ? { rejectUnauthorized: false } // Allows connections even without a valid certificate
+    : false, // No SSL
 });
 
 // Test the connection
 (async () => {
   try {
-    const res = await pool.query('SELECT NOW()');
-    console.log('Connected to PostgreSQL database. Server time:', res.rows[0].now);
+    await pool.query('SELECT NOW()');
+    console.log('Connected to PostgreSQL database.');
   } catch (err) {
     console.error('Failed to connect to PostgreSQL database:', err);
   }
 })();
 
 module.exports = pool;
+
+
+
+// set PGPASSWORD=ZexvzNYqrnEhh3HLA1s1xUNXEGNUUElL
+//psql -h dpg-ctqgfmi3esus73dtn7og-a.oregon-postgres.render.com -U store_management_db_dews_user -d store_management_db_dews
